32075728a964c1145cf3ce1d7b7b4367dacef068
chaoliu <chaoliu.is@gmail.com>
2023-01-27 17:39:41 +0800

    Add cmd

--- setup.py
+++ setup.py
@@ -19,6 +19,11 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'fib-number = chao_fib_py.cmd.fib_numb:fib_numb',
+        ],
+    },
     python_requires='>=3',
     tests_require=['pytest'],
 )
