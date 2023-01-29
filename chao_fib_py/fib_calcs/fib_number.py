ecc430ec90fff3216e2fe2568f4fae266aa373f8
chaoliu <chaoliu.is@gmail.com>
2023-01-29 10:11:30 +0800

    auto build

--- chao_fib_py/fib_calcs/fib_number.py
+++ chao_fib_py/fib_calcs/fib_number.py
@@ -1,14 +1,14 @@
 from typing import Optional
 
 
-def recurring_fibonacci_number(number: int) -> Optional[int]:
+def recurring_fibonacci_number(number: int) -> int:
     """
     Calculates the fibonacci number needed.
     :param number: (int) the Fibonacci number to be calculated
-    :return: (Optional[int]) the calculated fibonacci number
+    :return: (int) the calculated fibonacci number
     """
     if number < 0:
-        return None
+        raise ValueError("Fibonacci has to be equal or above zero")
     elif number <= 1:
         return number
     else:
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# trigger build (29-1-2023)
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
@@ -5,14 +6,20 @@ with open("README.md", "r") as fh:
 
 setup(
     name="chao_fib_py",
-    version="0.0.1",
+    version="0.0.3",
     author="Chao Liu",
     author_email="chaoliu.is@gmail.com",
     description="Calculates a Fibonacci number",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sjtu-liuchao/chao-fib-py",
-    install_requires=[],
+    install_requires=[
+        "PyYAML>=4.1.2",
+        "dill>=0.2.8"
+    ],
+    extras_require={
+     'server': ["Flask>=1.0.0"]
+    },
     packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta",
